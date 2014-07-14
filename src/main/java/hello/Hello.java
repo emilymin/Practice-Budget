29cb78ee8a7479ab014aedc601668af1d63a08ec
emily <emilymin1118@gmial.com>
2014-07-14 20:30:34 +0800

    successfully print log

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -5,7 +5,9 @@ import org.slf4j.LoggerFactory;
 
 public class Hello {
     public static String sayHello(){
+        System.out.println("Happy");
         Logger logger = LoggerFactory.getLogger("Hello.class");
+        logger.info("My name is Emily");
         logger.info("An info log message logged using SLF4j");
         return "Hello";
     }
--- src/test/java/hello/HelloTest.java
+++ src/test/java/hello/HelloTest.java
@@ -4,10 +4,9 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class HelloTest {
+public class HelloTest{
     @Test
-    public void sayHelloTest(){
-
+    public void testSayHello(){
         String result = Hello.sayHello();
 
         assertEquals("Hello",result);
